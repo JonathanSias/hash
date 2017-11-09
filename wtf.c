182e7bce9b0eec68941ab09aa974e2aa348df042
JonathanSias <jcsias@inf.ufpel.edu.br>
2017-11-09 15:05:47 -0200

    new file

---
+++ wtf.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// fator de carga para rehash
+int fatorDeCarga = 0;
+// numero posiçoes da hash
+int m = 0;
+// numero de chaves da hash
+int n = 0;
+// vetor char contendo palavra
+char palavra[99];
+// tamanho inicial da hash
+int tamanho = 256;
+
+
+
+// inicializaçao da hash
+void inicializar(){
+	//
+}
+
+// inserçao
+int insercao(){
+	//
+}
+
+// remoçao
+int remoçao(){
+	//
+}
+
+// busca
+int busca(){
+	//
+}
+
+// listar elementos
+void listar(){
+	//
+}
+
+// funçao hash
+int dispercao(){
+	//
+}
+
+// tratamento colisao
+int sondagemLinear(){
+	//
+}
+
+// rehash
+int rehash(){
+	//
+}
+
+// para letras minusculas
+int minusculas(){
+	//
+}
+
+int main(){
+	inicializar();
+	return 0;
+}
