de045fa84293f1463ace3c8395bb9e0ee452f2b0
Jonathan Sias <jcsias@inf.ufpel.edu.br>
2017-11-13 19:50:49 -0200

    update

--- wtf.c
+++ wtf.c
@@ -1,66 +1,146 @@
+// Jonathan Sias
+// 10205513
+// AEDII
+// Trabalho 1
+// Tabela Hash
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
 // fator de carga para rehash
-int fatorDeCarga = 0;
+float fatorDeCarga = 0.0;
 // numero posiçoes da hash
-int m = 0;
+int posicoes = 0;
 // numero de chaves da hash
-int n = 0;
+int chaves = 0;
 // vetor char contendo palavra
 char palavra[99];
 // tamanho inicial da hash
-int tamanho = 256;
-
-
+int tamanho = 997;
+// tabela hash
+struct tabela
+{
+	char *dado;
+}*tabelaHash;
+typedef struct tabela hash;
 
 // inicializaçao da hash
 void inicializar(){
-	//
+	tabelaHash = malloc(sizeof(hash)*tamanho);
+	int i;
+	for (i = 0; i < tamanho; ++i)
+	{
+		tabelaHash[i].dado = NULL;
+	}
 }
 
 // inserçao
 int insercao(){
 	//
+	return 0;
 }
 
 // remoçao
-int remoçao(){
+int remocao(){
 	//
+	return 0;
 }
 
 // busca
 int busca(){
 	//
+	return 0;
 }
 
 // listar elementos
 void listar(){
-	//
+	int i;
+	for (int i = 0; i < tamanho; ++i)
+	{
+		printf("%s\n", tabelaHash[i]);
+	}
 }
 
 // funçao hash
-int dispercao(){
-	//
+int dispercao(char *word){
+	int i;
+	int h = 0;
+	for (i = 0; word[i] != '\0'; ++i)
+	{
+		h = (h + word[i]);
+	}
+	h = (h % tamanho);
+	return h;
 }
 
-// tratamento colisao
+// tratando colisao
 int sondagemLinear(){
 	//
+	return 0;
 }
 
-// rehash
-int rehash(){
+// funçao rehash
+void rehash(){
 	//
 }
 
 // para letras minusculas
-int minusculas(){
+void minusculas(char *word){
+	int i = 0;
+	while(word[i] != '\0')
+	{
+		if (word[i] >= 'A' && word[i] <= 'Z')
+		{
+			word[i] = word[i] + 32;
+		}
+		i++;
+	}
+}
+
+// corrige palavra
+void correcao(){
 	//
 }
 
 int main(){
 	inicializar();
+	scanf("%s", palavra);
+	
+	/*
+	do{
+		// primaira leitura
+		scanf("%s", palavra);
+		// passando caracteres maiusculos para minusculos
+		minusculas(palavra);
+		// comparando tamanho da entrada
+		if (strlen(palavra) == 0)
+		{
+			printf("fail");
+		}else{
+			if (strlen(palavra) == 1)
+			{
+				if ((strcmp(palavra, "+") != 0) && (strcmp(palavra, "-") != 0) && (strcmp(palavra, "*") != 0))
+				{
+					printf("fail");
+				}
+			}else{
+				if (strcmp(palavra, "+") == 0)
+				{
+					//
+				}
+				if (strcmp(palavra, "-") == 0)
+				{
+					//
+				}
+				if (strcmp(palavra, "*"))
+				{
+					//
+				}
+			}
+		}
+	}while((strcmp(palavra, "*") != 0));
+	*/
 	return 0;
 }
